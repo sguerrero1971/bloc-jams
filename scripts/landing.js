f661264f2c26b66a59193e4466571d141b5ad5a3
Sergio Guerrero <sguerrero1971@gmail.com>
2016-11-16 08:27:11 -0500

    checkpoint-23_DOM_events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,12 +1,12 @@
-var animatePoints = function() {
+var pointsArray = document.getElementsByClassName('point');
 
-    var points = document.getElementsByClassName('point');
+var animatePoints = function(points) {
 
-    var revealPoint = function(item) {
-        points[item].style.opacity = 1;
-        points[item].style.transform = "scaleX(1) translateY(0)";
-        points[item].style.msTransform = "scaleX(1) translateY(0)";
-        points[item].style.WebkitTransform = "scaleX(1) translateY(0)";
+    var revealPoint = function(index) {
+        points[index].style.opacity = 1;
+        points[index].style.transform = "scaleX(1) translateY(0)";
+        points[index].style.msTransform = "scaleX(1) translateY(0)";
+        points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
     }
 
    for (i = 0; i < points.length; i++) {
@@ -14,3 +14,19 @@ var animatePoints = function() {
    }
 
 };
+
+window.onload = function() {
+
+    if (window.innerHeight > 950) {
+        animatePoints(pointsArray);
+    }
+
+    var sellingPoints = document.getElementsByClassName('selling-points');
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event) {
+        if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+            animatePoints(pointsArray);
+        }
+    });
+}
