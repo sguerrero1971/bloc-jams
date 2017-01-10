1683c30adf14c99d62d13c312893ace688bc5e0b
Sergio Guerrero <sguerrero1971@gmail.com>
2017-01-10 10:46:16 -0500

    assignment-25_DOM_album_view

--- scripts/album.js
+++ scripts/album.js
@@ -61,12 +61,13 @@ var createSongRow = function(songNumber, songName, songLength) {
     return template;
 };
 
+var albumTitle = document.getElementsByClassName('album-view-title')[0];
+var albumArtist = document.getElementsByClassName('album-view-artist')[0];
+var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
+var albumImage = document.getElementsByClassName('album-cover-art')[0];
+var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
+
 var setCurrentAlbum = function(album) {
-    var albumTitle = document.getElementsByClassName('album-view-title')[0];
-    var albumArtist = document.getElementsByClassName('album-view-artist')[0];
-    var albumReleaseInfo = document.getElementsByClassName('album-view-release-info')[0];
-    var albumImage = document.getElementsByClassName('album-cover-art')[0];
-    var albumSongList = document.getElementsByClassName('album-view-song-list')[0];
 
     albumTitle.firstChild.nodeValue = album.title;
     albumArtist.firstChild.nodeValue = album.artist;
@@ -81,5 +82,14 @@ var setCurrentAlbum = function(album) {
 };
 
 window.onload = function() {
-    setCurrentAlbum(albumPicasso);
+    setCurrentAlbum(albumEminem);
+
+    var albums = [albumPicasso, albumMarconi, albumEminem];
+    var index = 0;
+
+    albumImage.addEventListener('click', function (event) {
+        setCurrentAlbum(albums[index]);
+        index++;
+    });
 };
+
